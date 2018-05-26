01761450ce670a20ae61b504f65ad57747641688
Avani-13 <36336778+Avani-13@users.noreply.github.com>
2018-05-26 18:54:07 +0530

    Add files via upload

---
+++ Case Study 2/HbaseTable.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+ 
+public class HbaseTable {
+ 
+    private static Configuration conf = null;
+    /**
+     * Initialization
+     */
+    static {
+        conf = HBaseConfiguration.create();
+    }
+    /**
+     * Scan (or list) a table
+     */
+    public static void getAllRecord (String tableName) {
+        try{
+             HTable table = new HTable(conf, tableName);
+             Scan s = new Scan();
+             ResultScanner ss = table.getScanner(s);
+             for(Result r:ss){
+                 for(KeyValue kv : r.raw()){
+                    System.out.print(new String(kv.getRow()) + " ");
+                    System.out.print(new String(kv.getFamily()) + ":");
+                    System.out.print(new String(kv.getQualifier()) + " ");
+                    System.out.print(kv.getTimestamp() + " ");
+                    System.out.println(new String(kv.getValue()));
+                 }
+             }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void main(String[] agrs) {
+        try {
+            String tablename = "transactions";
+            String[] families = { "custid", "fname","count" };
+            System.out.println("===========show all record========");
+            HbaseTable.getAllRecord(tablename);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
